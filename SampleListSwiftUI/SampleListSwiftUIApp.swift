cbc158f511120eb5bcff15010f1586c26d41f110
vfj-tanaka <108640688+vfj-tanaka@users.noreply.github.com>
2022-09-20 15:07:55 +0900

    Initial Commit

---
+++ SampleListSwiftUI/SampleListSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleListSwiftUIApp.swift
+//  SampleListSwiftUI
+//
+//  Created by mtanaka on 2022/09/20.
+//
+
+import SwiftUI
+
+@main
+struct SampleListSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
