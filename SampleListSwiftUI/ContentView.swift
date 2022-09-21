e7d284257e899a9580ed17aaf7cdce7a08aa199b
vfj-tanaka <108640688+vfj-tanaka@users.noreply.github.com>
2022-09-21 15:25:38 +0900

    ep2

--- SampleListSwiftUI/ContentView.swift
+++ SampleListSwiftUI/ContentView.swift
@@ -17,20 +17,20 @@ struct ContentView: View {
     
     let players: [[Player]] = [
         [.init(id: 0, name: "James Harden", image: Image("def")),
-        .init(id: 1, name: "Stephen Curry", image: Image("def")),
-        .init(id: 2, name: "Lebron James", image: Image("def")),
-        .init(id: 3, name: "田中　誠", image: Image("def")),
-        .init(id: 4, name: "山田　太郎", image: Image("def"))],
+         .init(id: 1, name: "Stephen Curry", image: Image("def")),
+         .init(id: 2, name: "Lebron James", image: Image("def")),
+         .init(id: 3, name: "田中　誠", image: Image("def")),
+         .init(id: 4, name: "山田　太郎", image: Image("def"))],
         [.init(id: 0, name: "James Harden", image: Image("def")),
-        .init(id: 1, name: "Stephen Curry", image: Image("def")),
-        .init(id: 2, name: "Lebron James", image: Image("def")),
-        .init(id: 3, name: "田中　誠", image: Image("def")),
-        .init(id: 4, name: "山田　太郎", image: Image("def"))],
+         .init(id: 1, name: "Stephen Curry", image: Image("def")),
+         .init(id: 2, name: "Lebron James", image: Image("def")),
+         .init(id: 3, name: "田中　誠", image: Image("def")),
+         .init(id: 4, name: "山田　太郎", image: Image("def"))],
         [.init(id: 0, name: "James Harden", image: Image("def")),
-        .init(id: 1, name: "Stephen Curry", image: Image("def")),
-        .init(id: 2, name: "Lebron James", image: Image("def")),
-        .init(id: 3, name: "田中　誠", image: Image("def")),
-        .init(id: 4, name: "山田　太郎", image: Image("def"))]
+         .init(id: 1, name: "Stephen Curry", image: Image("def")),
+         .init(id: 2, name: "Lebron James", image: Image("def")),
+         .init(id: 3, name: "田中　誠", image: Image("def")),
+         .init(id: 4, name: "山田　太郎", image: Image("def"))]
     ]
     
     var body: some View {
@@ -41,17 +41,8 @@ struct ContentView: View {
                         .font(.system(size: 30))
                         .fontWeight(.heavy)
                     ) {
-                    ForEach(players[section]) { player in
-                            HStack {
-                                player.image
-                                    .resizable()
-                                    .scaledToFill()
-                                    .clipShape(Circle())
-                                    .frame(width: 50, height: 50)
-                                Text(player.name)
-                                    .font(.headline)
-                                    .padding(.leading, 20)
-                            }
+                        ForEach(self.players[section]) { player in
+                            PlayerRow(player: player, generation: self.switchHeaderTitle(section: section))
                         }
                     }.frame(height: 70)
                 }
@@ -70,6 +61,64 @@ struct ContentView: View {
         default:
             return ""
         }
+    }
+}
+
+struct PlayerRow: View {
+    
+    let player: Player
+    let generation: String
+    
+    var body: some View {
+        HStack {
+            NavigationLink(destination: PlayerDetail(player: player, generation: generation)) {
+                player.image
+                    .resizable()
+                    .scaledToFill()
+                    .clipShape(Circle())
+                    .frame(width: 50, height: 50)
+                Text(player.name)
+                    .font(.headline)
+                    .padding(.leading, 20)
+            }
+        }
+    }
+}
+
+struct PlayerDetail: View {
+    
+    let player: Player
+    let generation: String
+    
+    var body: some View {
+        NavigationView {
+            VStack {
+                player.image
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: 400, height: 400, alignment: .top)
+                    .cornerRadius(20)
+                    .padding(.init(top: 0, leading: 50, bottom: 0, trailing: 50))
+                HStack {
+                    Text(player.name)
+                        .font(.system(size: 30, weight: .heavy))
+                        .padding(.top, -85)
+                        .padding(.trailing, 20)
+                        .foregroundColor(.white)
+                }
+                .frame(width: 400, height: 60, alignment: .trailing)
+                HStack {
+                    Text("Generation: ")
+                        .font(.system(size: 30, weight: .medium))
+                    Text(generation)
+                        .font(.system(size: 40, weight: .heavy))
+                        .padding(.leading, 30)
+                }
+                .frame(width: 400, height: 40, alignment: .leading)
+                .padding(.leading, 20)
+                    Spacer()
+            }.padding(.top, 80)
+        }.navigationBarTitle(Text("Player"), displayMode: .inline)
     }
 }
 
